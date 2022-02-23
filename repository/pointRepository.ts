466a9976ab1946c2fe75cb79c00fa7bccd0d439b
jasmingeeks2connect <scgeeks2connect@gmail.com>
2022-02-23 22:22:18 +0530

    remove extra code

--- repository/pointRepository.ts
+++ repository/pointRepository.ts
@@ -1,57 +1,6 @@
 import { NuxtAxiosInstance } from "@nuxtjs/axios";
 import { PointRepo } from "~/types";
 
-const points = [
-  {
-    point: "2x",
-    title: "Comment on Twitter",
-  },
-  {
-    point: "3x",
-    title: "WEYU Top 5",
-  },
-  {
-    point: "1x",
-    title: "Like an instagram",
-  },
-  {
-    point: "2x",
-    title: "Share Twitter",
-  },
-  {
-    point: "1x",
-    title: "WEYU Top 5",
-  },
-  {
-    point: "3x",
-    title: "Share Twitter post",
-  },
-  {
-    point: "2x",
-    title: "Comment on Twitter",
-  },
-  {
-    point: "5x",
-    title: "Like Youtube video",
-  },
-  {
-    point: "2x",
-    title: "Comment on Twitter",
-  },
-  {
-    point: "3x",
-    title: "WEYU Top 5",
-  },
-  {
-    point: "1x",
-    title: "Like an instagram",
-  },
-  {
-    point: "2x",
-    title: "Share Twitter",
-  },
-];
-
 export default ($axios: NuxtAxiosInstance): PointRepo => ({
   async fetchPoints(params?: { page: number; per: number }) {
     try {
